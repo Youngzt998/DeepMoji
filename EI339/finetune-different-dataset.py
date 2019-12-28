97f3185adcb8a892f44316c1a811173129947247
youngzt <youngster@sjtu.edu.cn>
2019-12-28 07:13:54 -0800

    store some results

--- EI339/finetune-different-dataset.py
+++ EI339/finetune-different-dataset.py
@@ -2,38 +2,32 @@
 """
 from __future__ import print_function
 import sys
-import numpy as np
 from os.path import abspath, dirname
 sys.path.insert(0, dirname(dirname(abspath(__file__))))
 
 import json
-import math
+
 from deepmoji.model_def import deepmoji_transfer
-from deepmoji.global_variables import PRETRAINED_PATH, VOCAB_PATH
 from deepmoji.finetuning import (
     load_benchmark,
     finetune)
 from deepmoji.class_avg_finetuning import class_avg_finetune
 
 
-def roundup(x):
-    return int(math.ceil(x / 10.0)) * 10
-
-
-DATASETS = [
-    # ('SE0714', '../data/SE0714/raw.pickle', 3, True),
-    # ('Olympic', '../data/Olympic/raw.pickle', 4, True),
-    # ('PsychExp', '../data/PsychExp/raw.pickle', 7, True),
-    # ('SS-Twitter', '../data/SS-Twitter/raw.pickle', 2, False),
-    ('SS-Youtube', '../data/SS-Youtube/raw.pickle', 2, False),
-    # ('SE1604', '../data/SE1604/raw.pickle', 3, False), # Excluded due to Twitter's ToS
-    # ('SCv1', '../data/SCv1/raw.pickle', 2, True),
-    # ('SCv2-GEN', '../data/SCv2-GEN/raw.pickle', 2, True)
-]
-
+ROOT_PATH = dirname(dirname(abspath(__file__)))
+VOCAB_PATH = '{}/model/vocabulary.json'.format(ROOT_PATH)
+PRETRAINED_PATH = '{}/model/deepmoji_weights.hdf5'.format(ROOT_PATH)
 RESULTS_DIR = 'results'
 
-# 'new' | 'last' | 'full' | 'chain-thaw'
+training_parameters = [
+    #('SE0714', '../data/SE0714/raw.pickle', 3, True),
+    #('Olympic', '../data/Olympic/raw.pickle', 4, True),
+    # ('PsychExp', '../data/PsychExp/raw.pickle', 7, True),
+    ('SS-Twitter', '../data/SS-Twitter/raw.pickle', 2, False),
+    ('SS-Youtube', '../data/SS-Youtube/raw.pickle', 2, False),
+    ('SCv2-GEN', '../data/SCv2-GEN/raw.pickle', 2, True)
+]
+
 method = ['new', 'last', 'full', 'chain-thaw']
 FINETUNE_METHOD = 'last'
 VERBOSE = 1
@@ -46,18 +40,18 @@ with open(VOCAB_PATH, 'r') as f:
     vocab = json.load(f)
 
 for FINETUNE_METHOD in method:
-    if FINETUNE_METHOD != 'new':
+    if FINETUNE_METHOD != 'chain-thaw':
         continue
     for rerun_iter in range(1):
-        for p in DATASETS:
+        for training_parameter in training_parameters:
 
             # debugging
             assert len(vocab) == nb_tokens
 
-            dset = p[0]
-            path = p[1]
-            nb_classes = p[2]
-            use_f1_score = p[3]
+            dataset = training_parameter[0]
+            dataset_path = training_parameter[1]
+            nb_classes = training_parameter[2]
+            use_f1 = training_parameter[3]
 
             if FINETUNE_METHOD == 'last':
                 extend_with = 0
@@ -66,45 +60,41 @@ for FINETUNE_METHOD in method:
             else:
                 raise ValueError('Finetuning method not recognised!')
 
-            # Load dataset.
-            data = load_benchmark(path, vocab, extend_with=extend_with)
-
+            # Load dataset and split it into (train, validate, test)
+            data = load_benchmark(dataset_path, vocab, extend_with=extend_with)
             (X_train, y_train) = (data['texts'][0], data['labels'][0])
             (X_val, y_val) = (data['texts'][1], data['labels'][1])
             (X_test, y_test) = (data['texts'][2], data['labels'][2])
 
+            # load the prained dataset
             weight_path = PRETRAINED_PATH if FINETUNE_METHOD != 'new' else None
-            nb_model_classes = 2 if use_f1_score else nb_classes
+            nb_model_classes = 2 if use_f1 else nb_classes
             model = deepmoji_transfer(
                 nb_model_classes,
                 data['maxlen'], weight_path,
                 extend_embedding=data['added'])
             model.summary()
 
-            # Training
-            print('Training: {}'.format(path))
-            if use_f1_score:
-                model, result = class_avg_finetune(model, data['texts'],
-                                                   data['labels'],
-                                                   nb_classes, data['batch_size'],
-                                                   FINETUNE_METHOD,
-                                                   verbose=VERBOSE)
-            else:
-                model, result = finetune(model, data['texts'], data['labels'],
-                                         nb_classes, data['batch_size'],
-                                         FINETUNE_METHOD, metric='acc',
-                                         verbose=VERBOSE)
-
-            # Write results
-            if use_f1_score:
-                print('Overall F1 score (dset = {}): {}'.format(dset, result))
-                with open('{}/{}_{}_{}_results.txt'.
-                          format(RESULTS_DIR, dset, FINETUNE_METHOD, rerun_iter),
+            # fintune the model and run the benchmark
+            # then generate result files
+            print('Training: {}'.format(dataset_path))
+            if use_f1:
+                model, result = class_avg_finetune(model, data['texts'], data['labels'], nb_classes,
+                                                   data['batch_size'], FINETUNE_METHOD, verbose=VERBOSE)
+                print('Overall F1 score (dset = {}): {}'.format(dataset, result))
+                with open('{}/{}_{}_{}_f1_results.txt'.
+                          format(RESULTS_DIR, dataset, FINETUNE_METHOD, rerun_iter),
                           "w") as f:
                     f.write("F1: {}\n".format(result))
+
             else:
-                print('Test accuracy (dset = {}): {}'.format(dset, result))
-                with open('{}/{}_{}_{}_results.txt'.
-                          format(RESULTS_DIR, dset, FINETUNE_METHOD, rerun_iter),
+                model, result = finetune(model, data['texts'], data['labels'], nb_classes, data['batch_size'],
+                                         FINETUNE_METHOD, metric='acc', verbose=VERBOSE)
+                print('Test accuracy (dset = {}): {}'.format(dataset, result))
+                with open('{}/{}_{}_{}_acc_results.txt'.
+                          format(RESULTS_DIR, dataset, FINETUNE_METHOD, rerun_iter),
                           "w") as f:
                     f.write("Acc: {}\n".format(result))
+
+
+
